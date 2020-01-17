87ccfa101f187c122dbe59ce515c44d890a5c9e1
GrEy <barun.shrestha6@gmail.com>
2020-01-17 12:49:28 +0545

    commited

--- app/src/main/java/com/example/darazfinal/LoginActivity.java
+++ app/src/main/java/com/example/darazfinal/LoginActivity.java
@@ -24,9 +24,6 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getSupportActionBar().hide();
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_enter);
 
         btnExit=findViewById(R.id.btnExit);
